f6cd8f6d0946431992d5884944c17f3d4096ba0d
Loïc DUBOIS-TERMOZ <loic.duboistermoz@gmail.com>
2023-01-03 22:55:12 +0100

    refactor: add verifications to properties

--- patch-place-break/patch-place-break-storage/storage-api/src/main/java/fr/djaytan/minecraft/jobsreborn/patchplacebreak/internal/storage/api/properties/HostProperties.java
+++ patch-place-break/patch-place-break-storage/storage-api/src/main/java/fr/djaytan/minecraft/jobsreborn/patchplacebreak/internal/storage/api/properties/HostProperties.java
@@ -1,10 +1,12 @@
 package fr.djaytan.minecraft.jobsreborn.patchplacebreak.internal.storage.api.properties;
 
+import lombok.NonNull;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class HostProperties {
 
+  @NonNull
   String hostname;
   int port;
   boolean isSslEnabled;
