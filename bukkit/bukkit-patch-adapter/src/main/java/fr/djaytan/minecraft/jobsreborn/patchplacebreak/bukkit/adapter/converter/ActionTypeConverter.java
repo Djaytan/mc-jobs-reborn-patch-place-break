88b96cc4fb210209bb375f51e20d5ac10db06fb5
Loïc DUBOIS-TERMOZ <loic.duboistermoz@gmail.com>
2023-01-07 13:38:07 +0100

    chore: update copyright

--- bukkit/bukkit-patch-adapter/src/main/java/fr/djaytan/minecraft/jobsreborn/patchplacebreak/bukkit/adapter/converter/ActionTypeConverter.java
+++ bukkit/bukkit-patch-adapter/src/main/java/fr/djaytan/minecraft/jobsreborn/patchplacebreak/bukkit/adapter/converter/ActionTypeConverter.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 Loïc DUBOIS-TERMOZ (alias Djaytan)
+ * Copyright (c) 2022-2023 Loïc DUBOIS-TERMOZ (alias Djaytan)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
