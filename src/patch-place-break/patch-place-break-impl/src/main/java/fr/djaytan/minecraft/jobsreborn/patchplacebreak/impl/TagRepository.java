820075a422ab8f490fa6ad408535fe98a7a6e028
Djaytan <26904516+Djaytan@users.noreply.github.com>
2023-02-22 00:12:50 +0100

    build: reactivate javadoc generation

--- src/patch-place-break/patch-place-break-impl/src/main/java/fr/djaytan/minecraft/jobsreborn/patchplacebreak/impl/TagRepository.java
+++ src/patch-place-break/patch-place-break-impl/src/main/java/fr/djaytan/minecraft/jobsreborn/patchplacebreak/impl/TagRepository.java
@@ -35,12 +35,12 @@ import lombok.NonNull;
  * <p>A repository represents an abstraction of data storage. This is the appliance of the Data
  * Access Object pattern (DAO).
  *
- * @apiNote It is agnostic about whether the final storage is persistent (e.g. non-temporary files)
- *     or not (like in-memory). However, it is expected that the underlying implementation share
- *     ways to select the type of storage wished. At least one type must be a persistent one.
- *     Non-persistent ones are typically reserved for test purposes. If there isn't way to configure
- *     the type of storage, or no particular type wished as been provided, then the default type
- *     must be a persistent one.
+ * <p><i>Note:</i> It is agnostic about whether the final storage is persistent (e.g. non-temporary
+ * files) or not (like in-memory). However, it is expected that the underlying implementation share
+ * ways to select the type of storage wished. At least one type must be a persistent one.
+ * Non-persistent ones are typically reserved for test purposes. If there isn't way to configure the
+ * type of storage, or no particular type wished as been provided, then the default type must be a
+ * persistent one.
  */
 public interface TagRepository {
 
