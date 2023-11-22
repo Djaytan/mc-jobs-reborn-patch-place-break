aedb2fb84e300c5b444bf03ffd2f33a71d9a1cdf
Djaytan <26904516+Djaytan@users.noreply.github.com>
2023-11-22 12:14:06 +0100

    refactor(jpms): rename modules to ensure they will be globally unique
    Following the reverse-domain-name pattern which is a recommended practice (like it is the case for packages).

--- src/patch-place-break-core/src/main/java/module-info.java
+++ src/patch-place-break-core/src/main/java/module-info.java
@@ -20,9 +20,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-module jrppb.core {
+module fr.djaytan.mc.jrppb.core {
   // Internal dependencies
-  requires jrppb.api;
+  requires fr.djaytan.mc.jrppb.api;
 
   // General dependencies
   requires static org.jetbrains.annotations;
