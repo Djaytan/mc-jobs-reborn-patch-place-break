eaf4808487a757456669d94945f367bf9d2ea556
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-01-18 16:45:43 +0100

    build(packaging): factorize Maven arguments

--- .github/scripts/generate_spigot_plugin.sh
+++ .github/scripts/generate_spigot_plugin.sh
@@ -8,6 +8,8 @@ trap 'echo "Error encountered while executing the script." >&2' ERR
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P); readonly SCRIPT_DIR
 ROOT_REPOSITORY_DIR="$(realpath "${SCRIPT_DIR}/../..")"; readonly ROOT_REPOSITORY_DIR
 
+mvnw="${ROOT_REPOSITORY_DIR}/mvnw"
+
 readonly NEW_VERSION="$1"
 readonly DEV_VERSION='0.0.1-DEV-SNAPSHOT'
 
@@ -16,8 +18,8 @@ main() {
 
   cd "${ROOT_REPOSITORY_DIR}"
 
-  mvn versions:set -DnewVersion="${NEW_VERSION}" --batch-mode --errors
-  mvn clean package --activate-profiles fast --batch-mode --errors --strict-checksums
+  $mvnw versions:set -DnewVersion="${NEW_VERSION}"
+  $mvnw clean package
 
   local plugin_file_name
   plugin_file_name="$(mvn help:evaluate -Dexpression=spigotPlugin.finalName -DforceStdout --projects :spigot-plugin --quiet)"
@@ -31,7 +33,7 @@ main() {
 
   echo "The generated plugin file: '${plugin_file}'"
 
-  mvn versions:set -DnewVersion="${DEV_VERSION}" --batch-mode --errors
+  $mvnw versions:set -DnewVersion="${DEV_VERSION}"
 }
 
 main
